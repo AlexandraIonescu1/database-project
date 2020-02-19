ffba6fa005323f9dc8dc67ef938293274b42bc04
Alexandra Ionescu <ionescualexandra03@yahoo.com>
2020-02-19 13:56:23 +0200

    deleted the projects

--- DatabaseProjectConflictExample/sss/dbo/Views/AdventureErrorLogView.sql
+++ DatabaseProjectConflictExample/sss/dbo/Views/AdventureErrorLogView.sql
@@ -3,8 +3,6 @@ AS
 SELECT ErrorLogID, ErrorTime, UserName, ErrorNumber, ErrorSeverity, ErrorState, ErrorProcedure, ErrorLine, ErrorMessage
 FROM     [$(AdventureWorks2014Import)].dbo.ErrorLog
 
---SELECT * FROM AdventureWorks2014Import.dbo.ErrorLog
-
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane1', @value = N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
 Begin DesignProperties = 
